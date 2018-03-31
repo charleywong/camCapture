112cb29e6669f42449754565e068f369e05a5e72
Charley <woolies@charleys-macbook-air.local>
2018-03-31 14:13:22 +1100

    Initial Commit

---
+++ camCapture/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  camCapture
+//
+//  Created by Charley on 31/3/18.
+//  Copyright © 2018 Charley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
